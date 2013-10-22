cc3c7006e102bdbd666125001755ed301e73c7e2
Alexander Dormann <alexander.dormann@twt.de>
2013-10-22 11:52:35 +0200

    SQL now represents API duration

--- lib/patches/net_patches.rb
+++ lib/patches/net_patches.rb
@@ -3,9 +3,15 @@ if (defined?(Net) && defined?(Net::HTTP))
   Net::HTTP.class_eval do
     def request_with_mini_profiler(*args, &block)
       request = args[0]
-      Rack::MiniProfiler.step("Net::HTTP #{request.method} #{request.path}") do
-        request_without_mini_profiler(*args, &block)
-      end
+      desc = "Net::HTTP #{request.method} #{request.path}"
+      #Rack::MiniProfiler.step(desc) do
+        start = Time.now
+        result = request_without_mini_profiler(*args, &block)
+        elapsed_time = ((Time.now - start).to_f * 1000).round(1)
+        ::Rack::MiniProfiler.record_sql(desc, elapsed_time)
+      
+        result
+      #end
     end
     alias request_without_mini_profiler request
     alias request request_with_mini_profiler
